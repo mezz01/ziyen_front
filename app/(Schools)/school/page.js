ce1e58d6019ee7bf509a7a27239ba3fb0c1b9b07
Amine <mezzatamine2002@gmail.com>
2024-11-10 23:30:28 +0100

    putting up files structures

---
+++ app/(Schools)/school/page.js
@@ -0,0 +1,7 @@
+export default function School() {
+    return (
+        <div>
+            <h1 className="" >Hello this is the schools page</h1>
+        </div>
+    )
+}
---
+++ app/(User-profile)/components/module-card.js
@@ -0,0 +1,7 @@
+export default function ModuleCard(params) {
+    return(
+        <div className="Module-card ">
+            <h1> this is the C module</h1>
+        </div>
+    )
+}
